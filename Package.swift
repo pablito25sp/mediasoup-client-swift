6690b900044c41e64380bb4ace8ee82e82f3edb6
Pablo San Pietro <psanpietro@gmail.com>
2022-10-03 09:27:20 -0300

    Add WebRTC

--- Package.swift
+++ Package.swift
@@ -5,11 +5,18 @@ let package = Package(
   name: "mediasoup-client-swift",
   products: [
     .library(
+      name: "WebRTC",
+      targets: ["WebRTC"]),
+    .library(
       name: "Mediasoup",
       targets: ["Mediasoup"]),
   ],
   dependencies: [],
   targets: [
+    .binaryTarget(
+      name: "WebRTC",
+      path: "bin/WebRTC.xcframework"
+    ),
     .binaryTarget(
       name: "Mediasoup",
       path: "bin/Mediasoup.xcframework"
